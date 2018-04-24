74260f9501d314220678fdd7a93a9d2190950968
Paul Ogris <paul@tsahyt.com>
2018-04-24 20:03:17 +0200

    Add Struct module

---
+++ src/Graphics/IxShader/Struct.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fprint-explicit-kinds #-}
+
+module Graphics.IxShader.Struct where
+
+import Data.Proxy
+import Graphics.IxShader.Types
+import Graphics.IxShader.IxShader
+import Data.Promotion.Prelude.List
+import Data.Promotion.Prelude.Maybe
+
+import GHC.TypeLits
+
+newtype Struct (name :: Symbol) (fields :: [(Symbol, *)]) = Struct
+    { unStruct :: String
+    }
+
+instance forall name fields. KnownSymbol name =>
+         KnownTypeSymbol (Struct name fields) where
+    typeSymbolVal _ = symbolVal (Proxy @name)
+
+instance Socketed (Struct name fields) where
+    unSocket = unStruct
+    socket = Struct
+
+class FieldDeclarations (a :: [(Symbol, *)]) where
+    fieldDeclarations :: proxy a -> [String]
+
+instance FieldDeclarations '[] where
+    fieldDeclarations _ = []
+
+instance forall name typ xs. ( KnownSymbol name
+                             , KnownTypeSymbol typ
+                             , FieldDeclarations xs
+         ) =>
+         FieldDeclarations ('( name, typ) ': xs) where
+    fieldDeclarations _ = decl : fieldDeclarations (Proxy @xs)
+      where
+        decl =
+            "\t" ++
+            typeSymbolVal (Proxy @typ) ++ " " ++ symbolVal (Proxy @name) ++ ";"
+
+-- Test struct, delete this later
+{-
+ -type Material = 
+ -    Struct "Material" 
+ -        '[ '("ambient", Xvec3)
+ -         , '("diffuse", Xvec3)
+ -         , '("specular", Xvec3)
+ -         , '("shininess", Xfloat) ]
+ -}
+
+type family FieldAt (f :: Symbol) (fs :: [(Symbol, *)]) where
+    FieldAt f fs = 
+        FromMaybe (TypeError ('Text "Struct field not found during lookup")) (Lookup f fs)
+
+-- | Field accessor for structs
+field :: forall a n fs field.
+      (Socketed a, KnownSymbol n, KnownSymbol field, a ~ FieldAt field fs)
+   => Struct n fs
+   -> Proxy field
+   -> a
+field (Struct var) _ = socket $ var ++ "." ++ symbolVal (Proxy @field)
+
+-- | Introduce a new struct
+struct_ ::
+       forall name fields ts ctx shadertype.
+       ( FieldDeclarations fields
+       , KnownTypeSymbol (Struct name fields)
+       , Socketed (Struct name fields)
+       )
+    => IxShader shadertype ctx ts (ts :++ '[Struct name fields]) ()
+struct_ = acc decls (Struct "" :: Struct name fields) ()
+    where decls = unlines $
+            [ "struct " ++ typeSymbolVal (Proxy @(Struct name fields)) ++ "{" ] ++
+            fieldDeclarations (Proxy @fields) ++
+            [ "};" ]
